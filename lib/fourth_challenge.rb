265777ee9dceac598a1eaae1240c659c2cbc148f
Xiaoyi Liu <lxy878@gmail.com>
2020-03-06 14:06:00 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -7,14 +7,14 @@ def fourth_challenge
       :hero_friends => [
         #your hashes here!
         {
-          :name => "Steven",
-          :age => "30",
-          :attitude => "confused"
+         :name => "Benvolio",
+         :age => "17",
+         :attitude => "worried"
         },
         {
-          :name => "Nurse",
-          :age => "44",
-          :attitude => "worried"
+          :name => "Mercutio",
+          :age => "18",
+          :attitude => "hot-headed"
         }
       ]
    },
@@ -24,6 +24,16 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
+        {
+          :name => "Steven",
+          :age => "30",
+          :attitude => "confused"
+        },
+        {
+          :name => "Nurse",
+          :age => "44",
+          :attitude => "worried"
+        }
       ]
    }
 }
